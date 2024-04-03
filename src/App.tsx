115160fc1d451fd4e631ac5b7edc091dfd391414
Love Rumar Karlquist <lovebandy@hotmail.se>
2024-04-03 13:51:22 +0200

    Fungerande spel

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
 import Start from "./Components/Start";
 import Game from "./Components/Game";
 import Finished from "./Components/Finished";
+import useTicTacToe from "./useTicTacToe";
 
 function App() {
+  const game = useTicTacToe();
+
   return (
     <div>
       <h1>TicTacToe</h1>
-      <Game />
-      <Start />
-      <Finished />
+
+      {game.gameStatus === "started" && (
+        <Game board={game.board} handleClick={game.handleClick} />
+      )}
+      {game.gameStatus === "created" && (
+        <Start handleStart={game.handleStart} />
+      )}
+      {game.gameStatus === "finnished" && (
+        <Finished handleRestart={game.handleRestart} name={game.winner} />
+      )}
     </div>
   );
 }
--- src/Components/Game.tsx
+++ src/Components/Game.tsx
@@ -1,11 +1,34 @@
 import React from "react";
+import Square from "../Square";
 
-interface Props {}
+interface Props {
+  board: string[];
+  handleClick(index: number): void;
+}
 
 function Game(props: Props) {
+  const { board, handleClick } = props;
+
+  const styles = {
+    board: {
+      display: "grid",
+      gridTemplateColumns: "repeat(3, 1fr)",
+      width: "300px",
+    },
+  };
+
   return (
-    <div>
-      <h1>Game</h1>
+    <div style={styles.board}>
+      {board.map((value, index) => {
+        return (
+          <Square
+            value={value}
+            key={index}
+            index={index}
+            handleClick={handleClick}
+          />
+        );
+      })}
     </div>
   );
 }
